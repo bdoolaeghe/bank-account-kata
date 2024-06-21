d7de98b583c3fb063b1b5c7861c68ae6ce5fd0bc
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-06-21 19:35:53 +0200

    test red: add Amount.plus()

--- src/main/java/my/bank/account/Amount.java
+++ src/main/java/my/bank/account/Amount.java
@@ -6,4 +6,7 @@ record Amount(double value, Currency currency) {
         return new Amount(value, currency);
     }
 
+    public Amount plus(Amount added) {
+        throw new RuntimeException("implement me !");
+    }
 }
--- src/test/java/my/bank/account/AmountTest.java
+++ src/test/java/my/bank/account/AmountTest.java
@@ -1,24 +1,66 @@
 package my.bank.account;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static my.bank.account.Currency.EUR;
+import static my.bank.account.Currency.USD;
 import static org.assertj.core.api.Assertions.*;
 
 class AmountTest {
 
-    @ParameterizedTest
-    @ValueSource(doubles = {0, 10.15})
-    void amount_should_support_positive_value(double value) {
-        var amount = new Amount(value, Currency.EUR);
-        assertThat(amount).isNotNull();
+    @Nested
+    class AmountConsistency {
+
+        @ParameterizedTest
+        @ValueSource(doubles = {0, 10.15})
+        void amount_should_support_positive_value(double value) {
+            var amount = new Amount(value, EUR);
+            assertThat(amount).isNotNull();
+        }
+
+        @Test
+        void amount_should_support_negative_value() {
+            var amount = new Amount(-10, EUR);
+            assertThat(amount).isNotNull();
+        }
+
     }
 
-    @Test
-    void amount_should_support_negative_value() {
-        var amount = new Amount(-10, Currency.EUR);
-        assertThat(amount).isNotNull();
-    }
+    @Nested
+    class Plus {
 
+        @Test
+        void should_add_amounts_in_same_currency() {
+            // Given
+            var anEurAmount = new Amount(10, EUR);
+            // When
+            var newAmount = anEurAmount.plus(Amount.of(21.01, EUR));
+            // Then
+            assertThat(newAmount).isEqualTo(Amount.of(31.01, EUR));
+        }
+
+        @Test
+        void should_add_negative_amounts_in_same_currency() {
+            // Given
+            var anEurAmount = new Amount(10, EUR);
+            // When
+            var newAmount = anEurAmount.plus(Amount.of(-5, EUR));
+            // Then
+            assertThat(newAmount).isEqualTo(Amount.of(5, EUR));
+        }
+
+        @Test
+        void should_reject_adding_amounts_in_different_currencies() {
+            // Given
+            var anEurAmount = new Amount(10, EUR);
+            // When/Then
+            assertThatThrownBy(() ->
+                    anEurAmount.plus(Amount.of(21.01, USD))
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
 }
