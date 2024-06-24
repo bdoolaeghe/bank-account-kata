6077211a34cab9329e5b39305a7f9d314b20334a
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-06-24 16:28:22 +0200

    refactor: remove dead code Amount.plus() and minus()

--- src/main/java/my/bank/account/Amount.java
+++ src/main/java/my/bank/account/Amount.java
@@ -16,23 +16,6 @@ public record Amount(double value, Currency currency) {
         return new Amount(value, currency);
     }
 
-    Amount plus(Amount added) {
-        checkCurrencyIsSame(added);
-        return Amount.of(value + added.value, currency);
-    }
-
-    Amount minus(Amount subtracted) {
-        checkCurrencyIsSame(subtracted);
-        checkGreaterThanOrEquals(subtracted);
-        return Amount.of(value - subtracted.value, currency);
-    }
-
-    private void checkGreaterThanOrEquals(Amount another) {
-        if (another.gt(this)) {
-            throw new IllegalArgumentException("Can't subtract " + another + " because it's greater than current amount (" + this + ")");
-        }
-    }
-
     private void checkCurrencyIsSame(Amount anotherAmount) {
         if (anotherAmount.currency != currency) {
             throw new IllegalArgumentException("Amount " + anotherAmount + " has not same currency as " + this);
--- src/test/java/my/bank/account/AmountTest.java
+++ src/test/java/my/bank/account/AmountTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static my.bank.account.Currency.EUR;
-import static my.bank.account.Currency.USD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -30,63 +29,4 @@ class AmountTest {
 
     }
 
-    @Nested
-    class Plus {
-
-        @Test
-        void should_add_amounts_in_same_currency() {
-            // Given
-            var anEurAmount = new Amount(10, EUR);
-            // When
-            var newAmount = anEurAmount.plus(Amount.of(21.01, EUR));
-            // Then
-            assertThat(newAmount).isEqualTo(Amount.of(31.01, EUR));
-        }
-
-        @Test
-        void should_reject_adding_amounts_in_different_currencies() {
-            // Given
-            var anEurAmount = new Amount(10, EUR);
-            // When/Then
-            assertThatThrownBy(() ->
-                    anEurAmount.plus(Amount.of(21.01, USD))
-            ).isInstanceOf(IllegalArgumentException.class);
-        }
-
-    }
-
-    @Nested
-    class Minus {
-
-        @Test
-        void should_subtract_amounts_in_same_currency() {
-            // Given
-            var anEurAmount = new Amount(10, EUR);
-            // When
-            var newAmount = anEurAmount.minus(Amount.of(0.01, EUR));
-            // Then
-            assertThat(newAmount).isEqualTo(Amount.of(9.99, EUR));
-        }
-
-        @Test
-        void should_reject_subtracting_amounts_in_different_currencies() {
-            // Given
-            var anEurAmount = new Amount(10, EUR);
-            // When/Then
-            assertThatThrownBy(() ->
-                    anEurAmount.minus(Amount.of(0.01, USD))
-            ).isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @Test
-        void should_refuse_subtracting_higher_amount_than_initial_one() {
-            // Given
-            var anEurAmount = new Amount(10, EUR);
-            // When/Then
-            assertThatThrownBy(() ->
-                    anEurAmount.minus(Amount.of(1000, EUR))
-            ).isInstanceOf(IllegalArgumentException.class);
-        }
-
-    }
 }
